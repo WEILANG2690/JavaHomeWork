bb55895dbe220c3552c8a7fc5d6a900f1f593378
WEILANG2690 <william82628@outlook.com>
2017-06-28 07:50:27 +0800

    Add files via upload

---
+++ TestHome.java
@@ -0,0 +1,12 @@
+package WEILANG.JavaTest;
+
+public class TestHome {
+
+	public static void main(String[] args) {
+		int i=1, j=1;
+		for(i=1; i<=5; i++){
+		  for(j=1;j<=i;j++){
+		    System.out.print("*");
+		  }
+		  System.out.print("\n");
+		}}}
