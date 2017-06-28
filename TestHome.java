fc59f5096a6c95be752b7ff0ffc9bfc2cac6466f
WEILANG2690 <william82628@outlook.com>
2017-06-28 09:17:57 +0800

    Update TestHome.java

--- TestHome.java
+++ TestHome.java
@@ -4,8 +4,8 @@ public class TestHome {
 
 	public static void main(String[] args) {
 		int i=1, j=1;
-		for(i=1; i<=5; i++){
-		  for(j=1;j<=i;j++){
+		for(i=1; i<=5; i++){ //決定行數
+		  for(j=1;j<=i;j++){ //決定個數
 		    System.out.print("*");
 		  }
 		  System.out.print("\n");
